e4a2099d4a6724a72b8a2cb1c5fc29147f50cfaf
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-05-24 21:17:39 +0300

    pins definition

---
+++ src/pins.h
@@ -0,0 +1,32 @@
+#ifndef PINOUT_H_
+#define PINOUT_H_
+#define TX0 1
+#define D2 2
+#define RX0 3
+#define D4 4
+#define D5 5
+// from 6 - 11 flasher
+#define D12 12
+#define D13 13
+#define D14 14
+#define D15 15
+#define D14 14
+// #define RX2 16
+// #define TX2 17
+#define D18 18
+#define D19 19
+// D20 missing
+#define D21 21
+#define D22 22
+#define D23 23
+#define D25 25
+#define D26 26
+#define D27 27
+#define D32 32
+#define D33 33
+#define D34 34
+#define D35 35
+#define VP 36
+#define VN 39
+
+#endif
